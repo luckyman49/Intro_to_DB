9517bf1cd218bffa202899e1d9d4a7694e439f16
ganiyu lukman <lganiyu43@gmail.com>
2026-01-02 01:50:58 +0100

    Add new file

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,7 @@
--- TASK 5: INSERT A SINGLE ROW INTO CUSTOMERS TABLE
+-- TASK 5: INSERT A SINGLE ROW INTO CUSTOMER TABLE
 -- All SQL keywords are in UPPERCASE
 
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
